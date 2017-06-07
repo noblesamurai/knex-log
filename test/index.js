0c72d7a39be17df2bbb1bf50561e10d56ffa2be3
Tim Allen <tim@noblesamurai.com>
2017-06-07 16:15:10 +1000

    Parameterise test db conn string.

--- test/index.js
+++ test/index.js
@@ -2,16 +2,17 @@ const tape = require('tape');
 const it = require('tape-promise').default(tape);
 const knexLog = require('..');
 const test = require('abstract-log');
+const knex = process.env.TEST_DATABASE_URL || {
+  client: 'sqlite3',
+  connection: {
+    filename: './mydb.sqlite'
+  }
+};
 
 const common = {
   setup: async (t) => {
     return knexLog({
-      knex: {
-        client: 'sqlite3',
-        connection: {
-          filename: './mydb.sqlite'
-        }
-      },
+      knex,
       tableName: 'logs',
       _purgeLog: true
     });
